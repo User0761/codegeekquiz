4cef4bdc50aa6203c0bd2c418926c79958e67e16
DevasyaB <74359450+User0761@users.noreply.github.com>
2024-07-10 20:14:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+
+function colorChanger(){
+  var rgb_color;
+  rgb_color = ()=> {
+  var r;
+  var g;
+  var b;
+  var code;
+  r = Math.floor(Math.random() * 235);
+  g = Math.floor(Math.random() * 255);
+  b = Math.floor(Math.random() * 244);
+  code = "rgb(" + r + "," + g + "," + b + ")"
+  return code;
+ }
+document.bgColor = rgb_color();
+}
+function show(){
+  var answer="(A) With the Parliament";
+  document.getElementById("ans").innerHTML = answer;
+}
+function show1(){
+  var answer2="(A) Methane";
+  document.getElementById("ans1").innerHTML = answer2;
+}
+function show2(){
+  var answer3="(C) Railways";
+  document.getElementById("ans2").innerHTML = answer3;
+}
+function show3(){
+  var answer4="(A) Limestone";
+  document.getElementById("ans3").innerHTML = answer4;
+}
+function show4(){
+  var answer5="(C) Rabies virus";
+  document.getElementById("ans4").innerHTML = answer5;
+}
+function show5(){
+  var answer6="(C) President";
+  document.getElementById("ans5").innerHTML = answer6;
+}
+function show6(){
+  var answer7="(B) Extreme heat and pressure";
+  document.getElementById("ans6").innerHTML = answer7;
+}
+function show7(){
+  var answer8="(C) -40°";
+  document.getElementById("ans7").innerHTML = answer8;
+}
+function show8(){
+  var answer9="(A) North Atlantic Ocean ";
+  document.getElementById("ans8").innerHTML = answer9;
+}
+function show9(){
+  var answer10="(C) 11,033 m";
+  document.getElementById("ans9").innerHTML = answer10;
+}
+function show10(){
+  var answer11="(B) IIT-Gandhinagar";
+  document.getElementById("ans10").innerHTML = answer11;
+}
+function show11(){
+  var answer12="(C) Mission Shakti";
+  document.getElementById("ans11").innerHTML = answer12;
+}
+function show12(){
+  var answer13="(A) Admiral MA Hampiholi";
+  document.getElementById("ans12").innerHTML = answer13;
+}
+function show13(){
+  var answer14="(C) Mt Makalu";
+  document.getElementById("ans13").innerHTML = answer14;
+}
+function show14(){
+  var answer15="(C) IIT-Delhi";
+  document.getElementById("ans14").innerHTML = answer15;
+}
+function show15(){
+  var answer16="(B) 8";
+  document.getElementById("ans15").innerHTML = answer16;
+}
+function show16(){
+  var answer17="(D) Rs 5,000";
+  document.getElementById("ans16").innerHTML = answer17;
+}
+function show17(){
+  var answer18="(D) Morocco";
+  document.getElementById("ans17").innerHTML = answer18;
+}
+function show18(){
+  var answer19="(C) Israel";
+  document.getElementById("ans18").innerHTML = answer19;
+}
+function show19(){
+  var answer20="(B) Yaga Venugopal Reddy";
+  document.getElementById("ans19").innerHTML = answer20;
+}
